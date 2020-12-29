43132a26f7ab7f6db1ff19f3c8cb19e3dcebde21
lokesh12510 <lokeshkrishna50.com>
2020-12-29 16:08:04 +0530

    third

---
+++ navbar.js
@@ -0,0 +1,17 @@
+lled = () => {
+    dec = 10;
+    ec
+}
+ScrollTop = 0;
+ar = document.getElementById("navbar");
+ddEventListener("scroll", function() {
+            navbar.style.setProperty('background', scrolled() > 50 ? "#000000" : "#asasas")
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            llTop > lastScrollTop) {
+            ar.style.top = "-100px";
+
+        } else {
+            ar.style.top = "0";
+
+        }
+        lastScrllTop = scrollTop;
